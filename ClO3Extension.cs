5e68ac6b104d68e65f6f8da96e173d9faf8d969f
Furgo <furgos@qq.com>
2024-04-06 22:32:54 +0800

    aaaaaa

--- ClO3Extension.cs
+++ ClO3Extension.cs
@@ -15,5 +15,15 @@ public static class ClO3Extension
 	public static object? InvokeTMLMethod(string typeName, string methodName, object? invokeObj, object?[]? parameters = null) =>
 		GetTMLType(typeName).InvokeMethod(methodName, invokeObj, parameters);
 
+	public static object? GetFieldValue(this Type type, string name, object? typeObj) =>
+		type.GetField(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(typeObj);
 
+	public static object? GetFieldValue(this object obj, string name) =>
+		obj.GetType().GetField(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetValue(obj);
+
+	public static object? GetPropertyValue(this Type type, string name, object? typeObj) =>
+		type.GetProperty(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetMethod.Invoke(typeObj, null);
+
+	public static object? GetPropertyValue(this object obj, string name) =>
+		obj.GetType().GetProperty(name, BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic).GetMethod.Invoke(obj, null);
 }
--- DisplayErrorPatch.cs
+++ DisplayErrorPatch.cs
@@ -8,6 +8,7 @@ using Terraria.Social.Steam;
 using System.Collections.Generic;
 using System.Linq;
 using Terraria;
+using Version = System.Version;
 
 namespace ClO3;
 
@@ -43,11 +44,43 @@ public class DisplayErrorPatch : ModSystem
 			string path = Path.GetFullPath(WorkshopHelper.GetWorkshopFolder(new AppId_t(1281930u)));
 			path = Path.Combine(path, "content", "1281930");
 			var availableMods = ClO3Extension.InvokeTMLMethod("ModOrganizer", "FindMods", null, new object?[] { false }) as IEnumerable<object>;
-			// ClO3Extension.InvokeTMLMethod("ModOrganizer", "DeleteMod", null, new object?[] { availableMods[i] });
+			var responsibleModsObj = new List<object>();
+			if (availableMods != null)
+			{
+				foreach (object mod in availableMods)
+				{
+					if (mod.GetPropertyValue("Name") is not string modName || !responsibleMods.Contains(modName))
+						continue;
+					responsibleModsObj.Add(mod);
+				}
 
+				string modsStr = "";
+				foreach (var mod in responsibleModsObj)
+				{
+					modsStr += (mod.GetPropertyValue("Name") as string) + (mod.GetPropertyValue("Version") as Version) + ", ";
+				}
 
-			msg = "" +
-				msg;
+				string configPath = Path.Combine(Directory.GetParent(Main.SavePath).ToString(), "ModConfigs");
+				int configCount = 0;
+				foreach (string filePath in Directory.GetFiles(configPath))
+				{
+					if (responsibleMods.Contains(filePath.TakeWhile(c => c != '_')))
+					{
+						configCount++;
+						// File.Delete(filePath);
+					}
+				}
+
+				// ClO3Extension.InvokeTMLMethod("ModOrganizer", "DeleteMod", null, new object?[] { iii });
+				
+				modsStr = modsStr[..^2];
+
+				msg = "你的tModLoader在加载中崩溃了，已为你找到相关Mod：\n" +
+					  modsStr +
+					  $"为你自动删除相关Mod及{configCount}个配置文件" +
+					  msg;
+			}
+
 		}
 		orig.Invoke(msg, e, fatal, continueIsRetry);
 	}
